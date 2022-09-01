2971473d35ca74f5f1b6bbd5012d161679f554e6
xHeXifx <100049779+xHeXifx@users.noreply.github.com>
2022-09-02 00:18:39 +0100

    Add files via upload

--- temp/mover.bat
+++ temp/mover.bat
@@ -5,9 +5,9 @@ cd %userprofile%
 for /f %%i in (temp.txt) do set file=%%i
 
 move %file%\pyrax.bat %userprofile%\pyrax
+move %userprofile%\pyrax\pyrax.exe %userprofile%\Desktop
 
-cd %userprofile%\pyrax
-
-start /shared pyrax.bat
+cd %userprofile%\Desktop
+start pyrax.exe
 
 exit
